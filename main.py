90292981f07e9a0f93c5082323e8c14774100141
retr0-init <146389702+retr0-init@users.noreply.github.com>
2024-05-11 17:26:40 +0000

    View Summary TEST Draft

--- main.py
+++ main.py
@@ -744,7 +744,7 @@ class ModuleRetr0initConfinedTimeout(interactions.Extension):
             msg += f"\nPrisoners in {ctx.guild.get_channel(cid).mention}:\n"
             for i in pls:
                 timeleft: datetime.timedelta = datetime.datetime.now() - i.release_datetime.replace(tzinfo=None)
-                timestring: str = f"{timeleft.total_seconds / 60} minutes"
+                timestring: str = f"{timeleft.total_seconds() / 60} minutes"
                 msg += f"- {ctx.guild.get_member(i.id).mention} `{timestring} left`"
         pag: Paginator = Paginator.create_from_string(self.bot, f"Summary for Confined Timeout:\n\n{msg}", page_size=1000)
         await pag.send(ctx)
